5a9396f76b6fc8916f43b68f2736dbaf74c4794d
Shazly99 <ahmed.y.elshazly@gmail.com>
2022-05-07 19:47:14 +0200

    phone

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let img=document.getElementById('img');
+let body=document.body;
+function phones(phones){
+    img.src=phones;
+}
+
+function color(color){
+    body.style.backgroundColor=color;
+}
